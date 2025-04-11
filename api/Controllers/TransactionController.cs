4e22dbecdc8f3fe3c6a491ddd17cb1441741c524
Lucas Brown <lmbrown19@crimson.ua.edu>
2025-04-11 14:02:57 -0500

    Replacing everything

---
+++ api/Controllers/TransactionController.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TransactionController : ControllerBase
+    {
+        
+        // GET: api/<TransactionController>
+        [HttpGet]
+        public async Task<List<Transaction>> Get(){
+            Transaction T = new();
+            return await T.GetAllTransactionsAsync();
+        }
+
+        // GET api/<TransactionController>/{id}
+        [HttpGet("{id}", Name="Get")]
+        public async Task<Transaction> Get(string id)
+        {
+            Transaction T = new();
+            Transaction transaction = await T.GetATransactionAsync(id);
+        }
+
+        // POST api/<TransactionController>/{id}
+        [HttpPost]
+        public async Task Post([FromBody] Transaction value){
+            Transaction T = new();
+            await T.insertTransactionAsync();
+        }
+
+        // PUT api/<TransactionController>/{id}
+        public async Task Put(string id, [FromBody]){
+            // come back to this method
+            //used to update or completely erase a resource.
+            //not sure it is necessary for Transactions in this project
+        }
+
+        // DELETE api/<ShopController>/5
+        [HttpDelete("{id}")]
+        public async Task Delete(String id){
+            Transaction T = new();
+            await T.deleteTransactionAsync(id);
+        }
+    }
+}
