d346bbb755dd57fc15ce7bdea568c38d7b0aa822
Lucas Brown <lmbrown19@crimson.ua.edu>
2025-04-11 14:13:17 -0500

    added database string

--- api/Database/Database.cs
+++ api/Database/Database.cs
@@ -9,8 +9,7 @@ namespace API.Database
     {
         private const string cs;
         public Database(){
-            cs = "";
+            cs = "https://teams.microsoft.com/l/message/19:c8c5695af22143038e066f7592e4e9e9@thread.v2/1744396353268?context=%7B%22contextType%22%3A%22chat%22%7D";
         }
-        
     }
 }
