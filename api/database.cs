e395a9f3dbdfbd8e60fe5bb26a40b5b44b698084
Lucas Brown <lmbrown19@crimson.ua.edu>
2025-04-18 13:05:40 -0500

    Database class

---
+++ api/database.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Database
+{
+    public class database
+    {
+        public string cs;
+        public database(){
+            cs = "mysql://owzg1uln3090ua39:ba9c309h8qx6hddd@mkorvuw3sl6cu9ms.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/mvjb2fks5fyrys10";
+        }
+    }
+}
