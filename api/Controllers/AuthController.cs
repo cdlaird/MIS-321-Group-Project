1bd86c78c023754494ccca58ea6acb8092967b57
Ciara Laird <cdlaird@crimson.ua.edu>
2025-04-21 18:22:52 -0500

    debug

--- api/Controllers/AuthController.cs
+++ api/Controllers/AuthController.cs
@@ -18,18 +18,20 @@ namespace api.Controllers
 
         // POST: api/auth/register
         [HttpPost("register")]
-        public async Task<IActionResult> Register([FromBody] User user)
+        public IActionResult Register([FromBody] User user)
         {
-            await _auth.RegisterAsync(user.Username, user.Password);
+            _auth.Register(user.Username, user.Password);
             return Ok();
         }
 
         // POST: api/auth/login
         [HttpPost("login")]
-        public async Task<IActionResult> Login([FromBody] User user)
+        public ActionResult Login([FromBody] User user)
         {
-            bool valid = await _auth.LoginAsync(user.Username, user.Password);
-            return valid ? Ok("Login successful") : Unauthorized("Invalid credentials");
+            bool valid = _auth.Login(user.Username, user.Password);
+            return valid
+                ? Ok("Login successful")
+                : Unauthorized("Invalid credentials");
         }
     }
 }
--- api/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8f4a4295526c3939f7f3e99507d50f193e78c32")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad3511f457980456cec6b5e4c71a7d21ae2f1b51")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
