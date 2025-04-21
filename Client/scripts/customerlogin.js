63b45cbf51d3bd5650ccc52ad9333acecae31f0e
annahill32 <ahill69@crimson.ua.edu>
2025-04-21 16:09:43 -0500

    change to customer login

--- Client/scripts/customerlogin.js
+++ Client/scripts/customerlogin.js
@@ -1,7 +1,7 @@
-const url = ""; 
+const url = "";
 
 function showRegister() {
-  console.log('made it to register')
+  console.log("made it to register");
   document.getElementById("loginForm").classList.remove("active");
   document.getElementById("registerForm").classList.add("active");
 }
@@ -11,33 +11,36 @@ function showLogin() {
   document.getElementById("loginForm").classList.add("active");
 }
 
-
 const registerForm = document.getElementById("registerForm");
 
 registerForm.addEventListener("submit", async function (e) {
   e.preventDefault();
 
-  const firstName = document.getElementById("firstName").value;
-  const lastName = document.getElementById("lastName").value;
-  const phone = document.getElementById("phone").value;
-  const username = document.getElementById("newUsername").value;
-  const password = document.getElementById("newPassword").value;
+  firstName = document.getElementById("firstName").value;
+  lastName = document.getElementById("lastName").value;
+  phone = document.getElementById("phone").value;
+  username = document.getElementById("newUsername").value;
+  password = document.getElementById("newPassword").value;
+  points: 0;
+  tier: "Bronze";
 
   const customerData = {
     firstName,
     lastName,
     phone,
     username,
-    password
+    password,
+    points,
+    tier,
   };
 
   try {
     const response = await fetch(url, {
       method: "POST",
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(customerData)
+      body: JSON.stringify(customerData),
     });
 
     if (!response.ok) throw new Error("User registration failed");
@@ -45,11 +48,38 @@ registerForm.addEventListener("submit", async function (e) {
     alert("Account was created successfully!");
     showLogin();
     registerForm.reset();
-
   } catch (err) {
     console.error(err);
     alert("Something went wrong. Please try again.");
   }
 });
 
-//method to take to customer.html
+document.getElementById("loginForm").addEventListener("submit", async function (e){
+  e.preventDefault();
+  const user = {
+    username: document.getElementById("username").value,
+    password: document.getElementById("password").value
+  };
+
+  if(!user.username || !user.password){
+    alert("Enter username and password")
+    return;
+  }
+  try{
+    const response = await fetch(url, {
+      method: "POST",
+      headers: { "Content-Type" : "application/json" },
+      body: JSON.stringify(user)
+    }
+    )
+    if (response.ok) {
+      window.location.href = 'customer.html';
+    } else {
+      alert('Invalid username or password.');
+    }
+
+  } catch (error) {
+    console.error('Login error:', error);
+    alert('An error occurred while logging in.');
+  }
+});
